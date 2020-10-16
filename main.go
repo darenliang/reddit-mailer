cd61675956a24e06c6a6433346214f4f31420a13
Daren Liang <daren@darenliang.com>
2020-10-15 21:33:48 -0400

    Refactor and move global vars initialization to init

--- main.go
+++ main.go
@@ -10,106 +10,86 @@ import (
 	"github.com/turnage/graw/reddit"
 )
 
-var nomailIcon []byte
-var mailIcon []byte
+var (
+	noMailIcon []byte
+	mailIcon   []byte
+	mailCh     chan bool
+	exitCh     chan bool
+)
 
-var mailCh chan bool
-var exitCh chan int8
-
-func readFile(name string) ([]byte, error) {
-	f, err := os.Open(name)
-
-	if err != nil {
-		return nil, err
-	}
-
-	data, err := ioutil.ReadAll(f)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
-
-}
-
-func checkMail(bot reddit.Bot) (bool, error) {
-
-	h, err := bot.ListingWithParams("/message/unread", map[string]string{"limit": "1"})
-
-	if err != nil {
-		return false, err
-	}
-
-	return len(h.Messages) > 0, nil
-
-}
-
-func main() {
-
+func init() {
 	var err error
 
-	color.Green("Startup")
-
-	mailCh = make(chan bool)
-	exitCh = make(chan int8)
-
 	mailIcon, err = readFile("mail.ico")
-
 	if err != nil {
 		panic(err)
 	}
 
-	nomailIcon, err = readFile("nomail.ico")
-
+	noMailIcon, err = readFile("nomail.ico")
 	if err != nil {
 		panic(err)
 	}
 
 	color.Green("Images loaded")
 
+	mailCh = make(chan bool)
+	exitCh = make(chan bool)
+}
+
+func readFile(name string) ([]byte, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func checkMail(bot reddit.Bot) (bool, error) {
+	h, err := bot.ListingWithParams("/message/unread", map[string]string{"limit": "1"})
+	if err != nil {
+		return false, err
+	}
+
+	return len(h.Messages) > 0, nil
+}
+
+func main() {
+	color.Green("Startup")
+
 	go systray.Run(onReady, onExit)
 
-	var bot reddit.Bot
-
-	bot, err = reddit.NewBotFromAgentFile("agent.txt", 0)
-
+	bot, err := reddit.NewBotFromAgentFile("agent.txt", 0)
 	if err != nil {
 		panic(err)
 	}
 
 	color.Green("Reddit API initialized")
 
-	var mail bool
-
-	mail, err = checkMail(bot)
-
-	if err != nil {
-		panic(err)
-	}
-
-	mailCh <- mail
-
 	timer := time.NewTicker(15 * time.Second)
 
 	for {
 		select {
 		case <-timer.C:
-			mail, err = checkMail(bot)
+			mail, err := checkMail(bot)
 			if err != nil {
 				panic(err)
 			}
 			mailCh <- mail
 		case <-exitCh:
+			timer.Stop()
 			return
 		}
 	}
-
 }
 
 func onReady() {
-
-	systray.SetIcon(nomailIcon)
+	systray.SetIcon(noMailIcon)
 	systray.SetTitle("Reddit Mailer")
 
 	quit := systray.AddMenuItem("Quit", "Stop Reddit Mailer")
@@ -124,13 +104,12 @@ func onReady() {
 			if b {
 				systray.SetIcon(mailIcon)
 			} else {
-				systray.SetIcon(nomailIcon)
+				systray.SetIcon(noMailIcon)
 			}
 		}
 	}
-
 }
 
 func onExit() {
-	exitCh <- 1
+	exitCh <- true
 }
